f21d31772e635d23f9f4ee28ab00cb000ae3616e
Aleksey-7 <84351678+Aleksey-7@users.noreply.github.com>
2023-04-26 19:50:33 +0300

    add logo to homepage
    add logo to homepage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,11 @@
+import boy from '../images/Boy.png';
+import { LogoImg } from 'components/Tweet/Tweet.styled';
+
 export const HomePage = () => {
   return (
     <h1 style={{ marginTop: 200, textAlign: 'center', color: 'antiquewhite' }}>
       Wellcome to Tweets Space!
+      <LogoImg src={boy} alt="boy" width="35" />
     </h1>
   );
 };
--- src/pages/TweetsPage.jsx
+++ src/pages/TweetsPage.jsx
@@ -2,6 +2,8 @@ import { TweetsList } from 'components/TweetsList/TweetsList';
 import { Filter } from 'components/Filter/Filter';
 import { Link } from 'components/Navigation/Navigation.styled';
 import { useState } from 'react';
+import boy from '../images/Boy.png';
+import { LogoImg } from 'components/Tweet/Tweet.styled';
 
 export const TweetsPage = () => {
   const [filter, setFilter] = useState('');
@@ -12,6 +14,7 @@ export const TweetsPage = () => {
 
   return (
     <div style={{ position: 'relative' }}>
+      <LogoImg src={boy} alt="boy" width="18" />
       <Link to="/">Go Back</Link>
       <Filter filter={filter} handleChange={handleChange} />
       <TweetsList filter={filter} />
